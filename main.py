7ebcfae329097859a7582983af590b9d62153619
kellsonphilips <kellsonphilips@gmail.com>
2025-06-07 06:54:58 +0100

    followers and following error fix

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # Here we are creating a class of users like instagram users
 class User:
     # this is a constructor for our objects attribute initialization
-    def __init__(self, user_id, username, followers, following):
+    def __init__(self, user_id, username, followers = 0, following = 0):
         self.id = user_id
         self.username = username
-        self.followers = 0
-        self.following = 0
+        self.followers = followers
+        self.following = following
 
     # Here we can have a method to handle increment and decrement of the user followers
     def follower(self, user):
         user.followers += 1.0
         self.following += 1.0
 
-user_1 = User("001", "Philip", "0", "0")
-user_2 = User("002", "Chigozirim", "0", "0")
+user_1 = User("001", "Philip", 0, 0)
+user_2 = User("002", "Chigozirim", 0, 0)
 
 # user_1 followed user_2 , so we should be able to increment user_2 followers by 1 and increment user-1 following by 1
 user_1.follower(user_2)
