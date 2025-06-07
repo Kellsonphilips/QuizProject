052e80d031ad99372e69957b5935fcb31bed73a6
kellsonphilips <kellsonphilips@gmail.com>
2025-06-07 07:07:56 +0100

    follower back count fix

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ class User:
 
     # Here we can have a method to handle increment and decrement of the user followers
     def follower(self, user):
-        user.followers += 1.0
-        self.following += 1.0
+        user.followers += 1
+        self.following += 1
 
-user_1 = User("001", "Philip", 0, 0)
+user_1 = User("001", "Philip",   0, 0)
 user_2 = User("002", "Chigozirim", 0, 0)
 
 # user_1 followed user_2 , so we should be able to increment user_2 followers by 1 and increment user-1 following by 1
 user_1.follower(user_2)
+user_2.follower(user_1) # follow back
 
 print(user_1.id, user_1.username)
-print(user_1.followers, user_1.following)
+print(f"followers = {user_1.followers}", f"following = {user_1.following}")
 
 print(user_2.id, user_2.username)
-print(user_2.followers, user_2.following)
+print(f"followers = {user_2.followers}", f"following = {user_2.following}")
